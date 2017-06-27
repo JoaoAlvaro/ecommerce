0d832ae760e5f1ceafeb60ada1917134592f0d38
Trabalho <Trabalho>
2017-06-27 19:45:43 -0300

    Autoload joaoalvaro classes

--- vendor/joaoalvaro/php-classes/src/DB/Sql.php
+++ vendor/joaoalvaro/php-classes/src/DB/Sql.php
@@ -1,12 +1,12 @@
 <?php 
 
-namespace DB;
+namespace Softmake\DB;
 
 class Sql {
 
 	const HOSTNAME = "127.0.0.1";
 	const USERNAME = "root";
-	const PASSWORD = "1234";
+	const PASSWORD = "";
 	const DBNAME = "db_ecommerce";
 
 	private $conn;
