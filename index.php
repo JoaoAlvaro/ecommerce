b4c2de4849307cbb263eee511accf57805e44f7a
Trabalho <Trabalho>
2017-06-27 21:14:25 -0300

    Criação classe Page. Projeto com erro no draw

--- index.php
+++ index.php
@@ -2,17 +2,20 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use Softmake\Page;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
     
-	$sql = new Softmake\DB\Sql();
+	$page = new Page();
 	
-	$results = $sql->select("SELECT * FROM tb_users");
+	$page->setTpl("index");
 	
-	echo json_encode($results);
+	
 });
 
 $app->run();
