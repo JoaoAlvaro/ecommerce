02d916fef5552767250497dabe91883710de92df
joaoalvaro <joao.alvarocordeiro@icloud.com>
2017-09-10 21:28:20 -0300

    Atualizações tela de login

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php 
-
+session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
-use Softmake\Page;
+use \Softmake\Page;
+use \Softmake\PageAdmin;
+use \Softmake\Model\User;
 
 $app = new Slim();
 
@@ -14,9 +16,47 @@ $app->get('/', function() {
 	$page = new Page();
 	
 	$page->setTpl("index");
-	
-	
+		
 });
+
+$app->get('/administrador', function() {
+    
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+	
+	$page->setTpl("index");
+		
+});
+
+$app->get('/administrador/login', function() {
+    
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]); 
+
+	$page->setTpl("login");	
+
+});
+
+$app->post('/administrador/login', function() {
+    
+	User::login($_POST["deslogin"], $_POST["despassword"]);
+
+	header("Location: /admin");
+	exit;
+}); 
+
+$app->get('/administrador/logout', function() {
+    
+	User::logout();
+
+	header("Location: /administrador/login");
+	exit;
+});
+
+
 
 $app->run();
 
--- vendor/joaoalvaro/php-classes/src/Page.php
+++ vendor/joaoalvaro/php-classes/src/Page.php
@@ -9,26 +9,30 @@ class Page{
   private $tpl;
   private $options = [];
   private $defaults = [
+    "header"=>true,
+    "footer"=>true,
     "data"=>[]
   ];
 
-  public function __costruct($opts = array()){
+  public function __construct($opts = array(), $tpl_dir = "/views/"){
+
+    //s$this->defaults["data"]["session"] = $_SESSION;
 
     $this->options = array_merge($this->defaults, $opts);
 
     $config = array(
-  		"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/",
-  		"cache_dir"     => $SERVER["DOCUMENTO_ROOT"]."/views-cache/",
+  		"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"].$tpl_dir,
+  		"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
   		"debug"         => false
     );
 
     Tpl::configure( $config );
 
-    $this->$tpl = new Tpl;
+    $this->tpl = new Tpl;
 
     $this->setData($this->options["data"]);
 
-    $this->tpl->draw("header");
+   if($this->options["header"] === true) $this->tpl->draw("header");
 
   }
 
@@ -51,7 +55,7 @@ class Page{
 
   public function __destruct(){
 
-    $this->tpl->draw("footer");
+    if($this->options["footer"] === true) $this->tpl->draw("footer");
 
   }
 
